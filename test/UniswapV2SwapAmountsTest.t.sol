0ed69e4638a1c8541688738eb91a22c127440adc
0xRektified <bugislife@proton.me>
2025-06-08 21:45:46 +0800

    first commit

---
+++ test/UniswapV2SwapAmountsTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {IWETH} from "../interfaces/IWETH.sol";
+import {IUniswapV2Router02} from "../interfaces/IUniswapV2Router02.sol";
+
+contract UniswapV2SwapAmountsTest is Test {
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant UNISWAP_V2_ROUTER_02 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant MKR = 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2;
+
+    IWETH private constant weth = IWETH(WETH);
+    IUniswapV2Router02 private constant router = IUniswapV2Router02(UNISWAP_V2_ROUTER_02);
+
+    function test_SwapExactTokensForTokens() public view {
+        address[] memory path = new address[](3);
+        path[0] = WETH;
+        path[1] = DAI;
+        path[2] = MKR;
+
+        uint256 amountIn = 1e18;
+        uint256[] memory amounts = router.getAmountsOut(amountIn, path);
+        console2.log("WETH", amounts[0]);
+        console2.log("DAI", amounts[1]);
+        console2.log("MKR", amounts[2]);
+    }
+}
