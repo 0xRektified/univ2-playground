65a341b6e88a96a982667799ad24896413e571b4
0xRektified <bugislife@proton.me>
2025-06-08 22:58:11 +0800

    feat: add amountIn test

--- test/UniswapV2SwapAmountsTest.t.sol
+++ test/UniswapV2SwapAmountsTest.t.sol
@@ -15,7 +15,7 @@ contract UniswapV2SwapAmountsTest is Test {
     IWETH private constant weth = IWETH(WETH);
     IUniswapV2Router02 private constant router = IUniswapV2Router02(UNISWAP_V2_ROUTER_02);
 
-    function test_SwapExactTokensForTokens() public view {
+    function test_getAmountsOut() public view {
         address[] memory path = new address[](3);
         path[0] = WETH;
         path[1] = DAI;
@@ -23,6 +23,19 @@ contract UniswapV2SwapAmountsTest is Test {
 
         uint256 amountIn = 1e18;
         uint256[] memory amounts = router.getAmountsOut(amountIn, path);
+        console2.log("WETH", amounts[0]);
+        console2.log("DAI", amounts[1]);
+        console2.log("MKR", amounts[2]);
+    }
+
+    function test_getAmountsIn() public view {
+        address[] memory path = new address[](3);
+        path[0] = WETH;
+        path[1] = DAI;
+        path[2] = MKR;
+
+        uint256 amountOut = 1e15; // 0.001 MKR
+        uint256[] memory amounts = router.getAmountsIn(amountOut, path);
         console2.log("WETH", amounts[0]);
         console2.log("DAI", amounts[1]);
         console2.log("MKR", amounts[2]);
